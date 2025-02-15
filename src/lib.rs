b0c8abdd722b160ba96e329e4b653f8944c0f06a
qyct <rukmangadan.d@gmail.com>
2025-02-15 17:21:44 +0530

    p

--- src/lib.rs
+++ src/lib.rs
@@ -58,9 +58,6 @@ pub mod version;
 #[path = "../gdal/src/vsi.rs"]
 pub mod vsi;
 
-mod b;
-pub use b::build;
-
 #[cfg(test)]
 fn assert_almost_eq(a: f64, b: f64) {
     let f: f64 = a / b;
@@ -68,46 +65,48 @@ fn assert_almost_eq(a: f64, b: f64) {
     assert!(f > 0.99999);
 }
 
-// use std::{env, fs, path::PathBuf};
-// use reqwest::blocking::get;
-// use std::io::Write;
-// use sevenz_rust::decompress_file;
+// gdal_win::build()
 
-// pub fn build() {
-//     let out_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
-//     let profile = env::var("PROFILE").unwrap();
-//     let target_dir = PathBuf::from(&out_dir).join("target");
-//     let binary_dir = PathBuf::from(&target_dir).join(profile);
+use std::{env, fs, path::PathBuf};
+use reqwest::blocking::get;
+use std::io::Write;
+use sevenz_rust::decompress_file;
 
-//     let gdal_lib_url = "https://github.com/geocrate/files/releases/download/gdal-3100/gdal-3100-lib.7z";
-//     let gdal_bin_url = "https://github.com/geocrate/files/releases/download/gdal-3100/gdal-3100-bin.7z";
+pub fn build() {
+    let out_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+    let profile = env::var("PROFILE").unwrap();
+    let target_dir = PathBuf::from(&out_dir).join("target");
+    let binary_dir = PathBuf::from(&target_dir).join(profile);
+
+    let gdal_lib_url = "https://github.com/geocrate/files/releases/download/gdal-3100/gdal-3100-lib.7z";
+    let gdal_bin_url = "https://github.com/geocrate/files/releases/download/gdal-3100/gdal-3100-bin.7z";
     
-//     let lib_7z_file = target_dir.join("gdal-3100-lib.7z");
-//     let bin_7z_file = target_dir.join("gdal-3100-bin.7z");
+    let lib_7z_file = target_dir.join("gdal-3100-lib.7z");
+    let bin_7z_file = target_dir.join("gdal-3100-bin.7z");
     
-//     let gdal_lib_dir = target_dir.join("gdal-3100");
+    let gdal_lib_dir = target_dir.join("gdal-3100");
 
-//     // Ensure extract_path exists
-//     fs::create_dir_all(&gdal_lib_dir).unwrap();
+    // Ensure extract_path exists
+    fs::create_dir_all(&gdal_lib_dir).unwrap();
 
-//     // Download the file
-//     if !lib_7z_file.exists() || !bin_7z_file.exists() {
-//         let response = get(gdal_lib_url).unwrap();
-//         let mut file = fs::File::create(&lib_7z_file).unwrap();
-//         let content = response.bytes().unwrap();
-//         file.write_all(&content).unwrap();
+    // Download the file
+    if !lib_7z_file.exists() || !bin_7z_file.exists() {
+        let response = get(gdal_lib_url).unwrap();
+        let mut file = fs::File::create(&lib_7z_file).unwrap();
+        let content = response.bytes().unwrap();
+        file.write_all(&content).unwrap();
 
-//         let response = get(gdal_bin_url).unwrap();
-//         let mut file = fs::File::create(&bin_7z_file).unwrap();
-//         let content = response.bytes().unwrap();
-//         file.write_all(&content).unwrap();
+        let response = get(gdal_bin_url).unwrap();
+        let mut file = fs::File::create(&bin_7z_file).unwrap();
+        let content = response.bytes().unwrap();
+        file.write_all(&content).unwrap();
 
-//         decompress_file(&lib_7z_file, &gdal_lib_dir).unwrap();
-//         decompress_file(&bin_7z_file, &binary_dir).unwrap();
-//     }
+        decompress_file(&lib_7z_file, &gdal_lib_dir).unwrap();
+        decompress_file(&bin_7z_file, &binary_dir).unwrap();
+    }
 
-//     println!("cargo:rustc-link-lib=static=gdal_i");
-//     println!("cargo:rustc-link-search={}", gdal_lib_dir.to_str().unwrap());
+    println!("cargo:rustc-link-lib=static=gdal_i");
+    println!("cargo:rustc-link-search={}", gdal_lib_dir.to_str().unwrap());
     
-//     println!("cargo:rerun-if-changed=build.rs");
-// }
+    println!("cargo:rerun-if-changed=build.rs");
+}
